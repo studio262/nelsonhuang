371223d7e44e5a9b440c4d6b7bdf300905063ff2
Hunter Caron <huntercaron@mac.com>
2017-11-03 17:40:29 -0400

    working on static version

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,58 +2,75 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
+import styled from 'styled-components'
 
-import './index.css'
+//import './index.css'
+import '../assets/css/grid.css'
+import '../assets/css/animate.css'
+import '../assets/css/styles.css'
+
+import logo from '../assets/images/logo-2.png'
+
+const HeaderContainer = styled.nav`
+  width: 100%;
+  height: 60px;
+  top: 0;
+  position: fixed;
+  z-index: 100;
+
+  ul {
+      margin: 0;
+      padding-left: 4px;
+      right: 30px;
+      top: 15px;
+      position: fixed;
+      list-style: none;
+  }
+
+  ul li {
+      font-size: 10pt;
+      display: inline-block;
+      letter-spacing: 5px;
+  }
+
+  ul li a {
+      color: black;
+      text-decoration: none;
+  }
+
+  ul li a:hover {
+      color: grey;
+  }
+`
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
-    </div>
-  </div>
+  <HeaderContainer>
+      <Link to="/">
+          <img id="logo" src={logo}/>
+      </Link>
+      <ul>
+          <li><Link to="/about/">ABOUT</Link></li>
+      </ul>
+  </HeaderContainer>
 )
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Nelson Huang"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
+
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+
+    <div>
       {children()}
     </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   </div>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,105 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <main className="animated fadeInUp">
+        <div className="row push"></div>
+        <div className="row">
+            <div className="col-lg-offset-8 col-lg-3
+                        col-md-offset-7 col-md-5">
+                <a href="pages/colour.html">
+                    <img src="img/colour/colour-4.jpg"/>
+                </a>
+                <div className="title title-left">
+                    <h2>COLOUR</h2>
+                    <h3><div className="line"></div>01</h3>
+                </div>
+            </div>
+        </div>
+        <div className="row rise">
+            <div className="col-lg-offset-1 col-lg-3
+                        col-md-offset-1 col-md-4">
+                <a href="pages/beauty.html">
+                    <img src="img/beauty/beauty-1.jpg"/>
+                </a>
+                <div className="title title-right">
+                    <h2>BEAUTY</h2>
+                    <h3><div className="line"></div>02</h3>
+                </div>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-lg-offset-7 col-lg-2
+                        col-md-offset-7 col-md-3">
+                <a href="pages/portraits.html">
+                    <img src="img/portraits/portrait-1.jpg"/>
+                </a>
+                <div className="title title-right">
+                    <h2>PORTRAITS</h2>
+                    <h3><div className="line"></div>03</h3>
+                </div>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-lg-4
+                        col-md-4">
+                <a href="pages/crystal.html">
+                    <img src="img/crystal/crystal-1.jpg"/>
+                </a>
+                <div className="title title-right">
+                    <h2>CRYSTAL</h2>
+                    <h3><div className="line"></div>04</h3>
+                </div>
+            </div>
+        </div>
+        <div className="row rise">
+            <div className="col-lg-offset-8 col-lg-3
+                        col-md-offset-7">
+                <a href="pages/dissembled.html">
+                    <img src="img/dissembled/dissembled-2.jpg"/>
+                </a>
+                <div className="title title-left">
+                    <h2>DISSEMBLED</h2>
+                    <h3><div className="line"></div>05</h3>
+                </div>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-lg-offset-2 col-lg-2
+                        col-md-offset-2 col-md-3">
+                <a href="pages/eve.html">
+                    <img src="img/eve/eve-5.jpg"/>
+                </a>
+                <div className="title title-right">
+                    <h2>EVE</h2>
+                    <h3><div className="line"></div>06</h3>
+                </div>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-lg-offset-7 col-lg-4
+                        col-md-offset-7 col-md-4">
+                <a href="pages/zombieboy.html">
+                    <img src="img/zombie-boy/zombie-11.jpg"/>
+                </a>
+                <div className="title title-left">
+                    <h2>ZOMBIE BOY</h2>
+                    <h3><div className="line"></div>07</h3>
+                </div>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-lg-offset-1 col-lg-3 rise
+                        col-md-4">
+                <a href="pages/lucrece.html">
+                    <img src="img/lucrece/lucrece-3.jpg"/>
+                </a>
+                <div className="title title-right">
+                    <h2>LUCRECE</h2>
+                    <h3><div className="line"></div>08</h3>
+                </div>
+            </div>
+        </div>
+    </main>
   </div>
 )
 
