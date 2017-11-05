6362fedde0b6eb790330ae1763ae9d960f03505a
Hunter Caron <huntercaron@mac.com>
2017-11-05 10:12:44 -0500

    adding about page structure

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,53 @@
 import React from 'react'
-import Link from 'gatsby-link'
+import styled from 'styled-components'
 
-const AboutPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-  </div>
+const Container = styled.div`
+  margin-top: 90px;
+`
+
+const AboutText = styled.div`
+
+
+  p {
+    line-height: 1.6;
+  }
+`
+
+const PhoneNum = styled.p`
+
+`
+
+const Email = styled.p`
+
+`
+
+const AboutPage = ({data}) => (
+  <Container>
+
+    <AboutText dangerouslySetInnerHTML={{__html: data.contentfulAbout.aboutParagraph.aboutParagraph.replace(/(?:\r\n|\r|\n)/g, '<br />')}} />
+
+    <PhoneNum>
+      {data.contentfulAbout.phone}
+    </PhoneNum>
+
+    <Email>
+      {data.contentfulAbout.email}
+    </Email>
+
+  </Container>
 )
 
 export default AboutPage
+
+export const query = graphql`
+  query AboutQuery {
+    contentfulAbout(title: { eq: "About"} ) {
+      phone
+      email
+
+      aboutParagraph {
+        aboutParagraph
+		  }
+  	}
+  }
+`
